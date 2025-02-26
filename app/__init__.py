c8927fc5ebf1ea108cbf75eb01256c1af2ed8903
VincentDesmouceaux <desmontvincent@gmail.com>
2025-02-26 14:16:39 +0100

    preparation du master pour basculement sur la branche QA

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,1 @@
 # app/booking_manager/__init__.py
-from .booking_service import BookingService
-from .club_manager import ClubManager
-from .competition_manager import CompetitionManager
-from .data_loader import JSONDataLoader
